8377b3c213f7188cf675716323b725981283005e
Drozd <drozd.gj@gmail.com>
2021-11-20 14:51:46 +0100

    InitialCreate_Extended
    new entities added, product repository, and few more

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+        public ProductsController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<ActionResult<Product>> GetProduct(int productId)
+        {
+            return await _context.Products.FindAsync(productId);
+        }
+    }
+}
---
+++ Business/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreDbContext _context;
+
+        public ProductRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProductByIdAsync(int productId)
+        {
+            var product = await _context.Products.FindAsync(productId);
+
+            return product;
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
+        }
+    }
+}
