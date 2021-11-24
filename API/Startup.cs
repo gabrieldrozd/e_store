c7d965b2794cb2c07c60b1145d3f92f38822297a
Drozd <drozd.gj@gmail.com>
2021-11-24 21:05:52 +0100

    Added Cors
    Added Cors Policy and usage

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,13 @@ namespace API
             // Add services and swagger
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +46,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
